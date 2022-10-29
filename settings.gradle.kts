ce8f77a8e8bbbf96fb1007ecc4e613adb0537210
髙橋和晃 <kazuaki.flute@gmail.com>
2022-10-29 23:14:46 +0900

    初期生成ファイルのアップロード

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "kazuchanfl-atcoder"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    readLine()!!
+    val hs = readLine()!!.split(" ").map(String::toInt)
+    println(hs.indexOf(hs.max()) + 1)
+}
